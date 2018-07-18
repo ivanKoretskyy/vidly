648becd532ae3f5823bd3ce12b53a2e7cff4972f
ivanKoretskyy <ivan.koretskyy@cgm.com>
2018-07-18 17:01:57 +0300

    remove start script

--- startup/config.js
+++ startup/config.js
@@ -4,6 +4,6 @@ const config = require('config');
 module.exports = function() {
   //make sure we have config from environment variables if no - exit
   if( !config.get('jwtPrivateKey')) {
-    throw new Error('Fatal error: jwtPrivateKey not defined') 
+    //throw new Error('Fatal error: jwtPrivateKey not defined') 
   }
 }
