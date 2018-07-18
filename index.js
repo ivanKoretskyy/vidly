8b0b1d080afcd72fe93534ff2322168a949a29ea
ivanKoretskyy <ivan.koretskyy@cgm.com>
2018-07-18 15:27:11 +0300

    add configuratioin for heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/routes')(app);
 require('./startup/dbconnection')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, "localhost", () => {
