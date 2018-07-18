ad21656d2c60c40f29b3642bc5846c61cac8e837
ivanKoretskyy <ivan.koretskyy@cgm.com>
2018-07-18 17:11:03 +0300

    disable mongo logging

--- startup/logger.js
+++ startup/logger.js
@@ -5,7 +5,7 @@ module.exports = function() {
   winston.add(new winston.transports.File({
     filename: 'logger.log'
   }));
-   winston.add(new winston.transports.MongoDB( {db: "mongodb://localhost:27017/vidly"}));
+   //winston.add(new winston.transports.MongoDB( {db: "mongodb://localhost:27017/vidly"}));
   winston.add(new winston.transports.Console({
     prettyPrint: true,
     colorize: true
