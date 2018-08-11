a7caa0c1554b1f25b9f0d07996487646f847f3f6
ivankoretskyy <ivankoretskyy@gmail.com>
2018-08-11 03:58:51 +0300

    add support for cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('./startup/dbconnection')();
-require('./startup/logger')();
-require('./startup/routes')(app);
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+app.use(cors());
+require("./startup/dbconnection")();
+require("./startup/logger")();
+require("./startup/routes")(app);
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
-console.log('listen on port ' + port)
+console.log("listen on port " + port);
 app.listen(port);
